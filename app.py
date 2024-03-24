1bc804ffcb897c4e57d13e441aab6fbe9c97c027
sabak03 <sabaykhan@hotmail.com>
2024-03-24 22:01:31 +0000

    Working application

--- app.py
+++ app.py
@@ -1,32 +1,40 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 import os
 import librosa
 import numpy as np
 from keras.models import load_model
 from werkzeug.utils import secure_filename
-from tempfile import gettempdir  
+from pydub import AudioSegment
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 CORS(app)  
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
 
 model = load_model('khaaModel.h5')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
 
 @app.route('/upload-audio', methods=['POST'])
 def uploadAudio():
     if 'audio' not in request.files:
         return jsonify({'message': 'No file part'}), 400
-
     audioFile = request.files['audio']
     if audioFile.filename == '':
         return jsonify({'message': 'No selected file'}), 400
-
     if audioFile and isAllowedFile(audioFile.filename):
         secureFilename = secure_filename(audioFile.filename)
-        filePath = os.path.join(gettempdir(), secureFilename) 
+        filePath = os.path.join(UPLOAD_FOLDER, secureFilename)
         audioFile.save(filePath)
         return processAudioFile(filePath)
-
     return jsonify({'message': 'Invalid file'}), 400
 
 def isAllowedFile(filename):
@@ -38,15 +46,19 @@ def extractFeatures(filePath):
     mfccsProcessed = np.mean(mfccs.T, axis=0)
     return mfccsProcessed.reshape(1, -1)
 
+
 def processAudioFile(filePath):
-    features = extractFeatures(filePath)
+    original_audio = AudioSegment.from_file(filePath)
+    converted_path = filePath.replace(".wav", "_converted.wav")
+    original_audio.export(converted_path, format="wav")
+    features = extractFeatures(converted_path)
     prediction = model.predict(features)
     isCorrect = prediction >= 0.5
     result = 'Correct' if isCorrect else 'Incorrect'
-    
-    os.remove(filePath)
-    
+    # Remove the converted file after processing
+    if os.path.exists(converted_path):
+        os.remove(converted_path)
     return jsonify({'pronunciation': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)  
+    app.run(debug=True)
